78d519f80f6238211a174f8b153bee7217fd3bb2
Belov Roman <otezzz1982@gmail.com>
2022-04-04 18:33:37 +0300

    React test app

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,41 @@
+import ExpenseItem from './ExpenseItem'
+import Card from '../UI/Card'
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter'
+
+import './Expenses.css'
+
+function Expenses (props) {
+    const filterExpenseHandler = (filterValue) => {
+        console.log(filterValue);
+    };
+
+    return (
+        <div>
+            <ExpensesFilter onFilterExpenseHandler={filterExpenseHandler} />
+            <Card className="expenses">
+                <ExpenseItem
+                    title={props.expensesData[0].title}
+                    amount={props.expensesData[0].amount}
+                    date={props.expensesData[0].date}>
+                </ExpenseItem>
+                <ExpenseItem
+                    title={props.expensesData[1].title}
+                    amount={props.expensesData[1].amount}
+                    date={props.expensesData[1].date}>
+                </ExpenseItem>
+                <ExpenseItem
+                    title={props.expensesData[2].title}
+                    amount={props.expensesData[2].amount}
+                    date={props.expensesData[2].date}>
+                </ExpenseItem>
+                <ExpenseItem
+                    title={props.expensesData[3].title}
+                    amount={props.expensesData[3].amount}
+                    date={props.expensesData[3].date}>
+                </ExpenseItem>
+            </Card>
+        </div>
+    );
+}
+
+export default Expenses;
