ce681198c931d5afbdc08721f3ec184e57c9baed
Belov Roman <otezzz1982@gmail.com>
2022-04-17 17:45:16 +0300

    Practise app

--- src/components/Modal/ErrorModal.js
+++ src/components/Modal/ErrorModal.js
@@ -6,22 +6,20 @@ import styles from './ErrorModal.module.css';
 const ErrorModal = props => {
 
     return (
-        {props.modalHandler {
-            return (
-                <div>
-                    <div className={styles.backdrop}></div>
-                    <div className={styles.modal}>
-                        <div className={styles.header}>
-                            <h2>Invalid input</h2>
-                        </div>
-                        <div className={styles.content}></div>
-                        <div className={styles.actions}>
-                            <Button type="button">Okay</Button>
-                        </div>
-                    </div>
+        {!props.modalHandler && return();}
+
+        <div>
+            <div className={styles.backdrop}></div>
+            <div className={styles.modal}>
+                <div className={styles.header}>
+                    <h2>Invalid input</h2>
                 </div>
-            );
-        };
+                <div className={styles.content}></div>
+                <div className={styles.actions}>
+                    <Button type="button">Okay</Button>
+                </div>
+            </div>
+        </div>
     );
 };
 
