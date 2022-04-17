a6f85fe14aed9738434313ce0ab25dd07c6b57b2
Belov Roman <otezzz1982@gmail.com>
2022-04-17 17:35:17 +0300

    Practise app

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,10 +1,21 @@
 import React, {useState} from 'react';
 
 import Button from '../../UI/Button/Button';
+import ErrorModal from '../../Modal/ErrorModal';
 import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
-    const [enteredText, setEnteredText] = useState('');
+    const [userText, setUserText] = useState('');
+    const [ageText, setAgeText] = useState('');
+    const [useModal, setUseModal] = useState('false');
+
+    const userHandler = (event) => {
+        setUserText(event.target.value);
+    };
+
+    const ageHandler = (event) => {
+        setAgeText(event.target.value);
+    };
 
     const formSubmitHandler = event => {
         event.preventDefault();
@@ -14,20 +25,26 @@ const CourseInput = props => {
         const age =event.target.elements['age'].value;
         const compiledText = userName + `${' (' + age + ' years old)'}`;
 
+        if (userName === undefined) {
+            setUseModal(true);
+        }
+
         props.compText(compiledText);
-        setEnteredText('');
+        setUserText('');
+        setAgeText('');
     };
 
     return (
         <form onSubmit={formSubmitHandler}>
             <div className={`${styles['form-control']}`}>
                 <label>Username</label>
-                <input value={enteredText} type="text" id="username"/>
+                <input value={userText} type="text" id="username" onChange={userHandler}/>
                 <br/>
                 <label>Age (Years)</label>
-                <input value={enteredText} type="number" id="age"/>
+                <input value={ageText} type="number" id="age" onChange={ageHandler}/>
             </div>
             <Button type="submit">Add User</Button>
+            <ErrorModal modalHandler={useModal}/>
         </form>
     );
 };
---
+++ src/components/Modal/ErrorModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Button from '../UI/Button/Button'
+import styles from './ErrorModal.module.css';
+
+const ErrorModal = props => {
+
+    return (
+        {props.modalHandler {
+            return (
+                <div>
+                    <div className={styles.backdrop}></div>
+                    <div className={styles.modal}>
+                        <div className={styles.header}>
+                            <h2>Invalid input</h2>
+                        </div>
+                        <div className={styles.content}></div>
+                        <div className={styles.actions}>
+                            <Button type="button">Okay</Button>
+                        </div>
+                    </div>
+                </div>
+            );
+        };
+    );
+};
+
+export default ErrorModal;
